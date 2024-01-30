57704ee98e6250d6a24bc360c71658ff03323efc
Azamat Mamarajabov <azamatjoon77@gmail.com>
2024-01-30 23:20:14 +0500

    Create Student controller,livewire,blade,route...

--- app/Models/Classes.php
+++ app/Models/Classes.php
@@ -2,8 +2,8 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Classes extends Model
 {
@@ -12,7 +12,6 @@ class Classes extends Model
     protected $fillable = [
         'name',
     ];
-
 
     public function sections()
     {
